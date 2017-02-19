3ad4b79f4081dfc6cffb9e4f49fcbf75bc73f801
DanRezo <DrEzO@DaNReZO.local>
2017-02-19 15:31:27 -0700

    Week 3 porject submission

--- src/cards/StarCityCasino.java
+++ src/cards/StarCityCasino.java
@@ -4,6 +4,6 @@ public class StarCityCasino {
 	public static void main(String[] args) throws InterruptedException {
 		House houseFloor = new House();
 		houseFloor.startGame();
-		//test
+		
 	}
 }
