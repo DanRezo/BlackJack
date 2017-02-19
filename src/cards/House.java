1a75c3e7db4243f4a784421edc82746b205c6efc
DanRezo <DrEzO@DaNReZO.local>
2017-02-18 17:08:16 -0700

    Updated House Class with animations and BlackJack win method

--- src/cards/House.java
+++ src/cards/House.java
@@ -3,9 +3,7 @@ package cards;
 import java.util.Scanner;
 
 public class House {
-	public static void main(String[] args) {
-
-		
+	public static void main(String[] args) throws InterruptedException {
 
 		// Double wager;
 		Scanner kb = new Scanner(System.in);
@@ -44,12 +42,18 @@ public class House {
 		String nm = kb.next();
 		if (choice.equalsIgnoreCase("y")) {
 			player.setName(nm);
-			System.out.println("Salutaions for the semi-circle.");
-			System.out.println("*********"+ player.getName()+"*********");
+			System.out.println("Salutaions for the Semi-Circle.");
+			System.out.println("*********" + player.getName() + "*********");
+			Thread.sleep(1800);
 			System.out.println("Name of the game is BlackJack.");
+			Thread.sleep(1650);
 			System.out.println("Dealer hits on 16 and stays on 17.");
+			Thread.sleep(1650);
 			System.out.println("Ready?");
+			Thread.sleep(1650);
 			System.out.println("Here come the cards.");
+			System.out.println("\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\");
+			Thread.sleep(3000);
 
 		}
 
@@ -61,25 +65,44 @@ public class House {
 		d.deal(dealer);
 		d.deal(player);
 		d.deal(dealer);
+
 		showHand(player);
 		System.out.println("Total " + player.getPlayerHand().getValueOfHand());
+		System.out.println("*****************************");
 		showHand(dealer);
 		System.out.println("Total " + dealer.getPlayerHand().getValueOfHand());
+		System.out.println("*****************************");
+		if(player.getPlayerHand().getValueOfHand()==21 && dealer.getPlayerHand().getValueOfHand()!=21){
+			System.out.println("BlackJack" + player.getName() + "Wins");
+			keepGoing = false;
+		}
+		else if(player.getPlayerHand().getValueOfHand()!=21 && dealer.getPlayerHand().getValueOfHand()==21){
+			System.out.println("BlackJack Dealer Wins!!");
+			keepGoing = false;
+		}
+		else if(player.getPlayerHand().getValueOfHand()==21 && dealer.getPlayerHand().getValueOfHand()==21){
+			System.out.println("Push");
+			keepGoing = false;
+		}
+		
 		while (keepGoing) {
 
 			while (playerKeepGoing) {
 				System.out.println("Do you want to hit or stay");
 				String input = kb.next();
 				if (input.equalsIgnoreCase("hit")) {
+					System.out.println("*****************************");
 					d.deal(player);
 					showHand(player);
 					System.out.println("Total " + player.getPlayerHand().getValueOfHand());
+					System.out.println("*****************************");
 					showHand(dealer);
 					System.out.println("Total " + dealer.getPlayerHand().getValueOfHand());
 					if (player.getPlayerHand().getValueOfHand() <= 21) {
 						continue;
-					} else {
-						System.out.println("You busted!!");
+					}
+					else {
+						System.out.println("You busted! Dealer Wins.");
 						playerKeepGoing = false;
 						dealerKeepGoing = false;
 					}
@@ -94,43 +117,39 @@ public class House {
 					d.deal(dealer);
 					showHand(player);
 					System.out.println("Total " + player.getPlayerHand().getValueOfHand());
-
+					System.out.println("*****************************");
 					showHand(dealer);
 					System.out.println("Total " + dealer.getPlayerHand().getValueOfHand());
-
-				} else if (dealer.getPlayerHand().getValueOfHand() > 21) {
-					System.out.println("Dealer BUST");
+					System.out.println("*****************************");
+				}
+				else if (dealer.getPlayerHand().getValueOfHand() > 21) {
+					System.out.println("Dealer BUST!");
 				} else {
-					dealerKeepGoing = false;
 					checkForWinner(player, dealer);
+					dealerKeepGoing = false;
 				}
 			}
 
 			kb.close();
-		
-}
+
+		}
 	}
 
-		public static void checkForWinner(Player player, Player dealer) {
-			if (player.getPlayerHand().getValueOfHand() > dealer.getPlayerHand().getValueOfHand()) {
-				System.out.println("Player wins!");
-			} else if (player.getPlayerHand().getValueOfHand() < dealer.getPlayerHand().getValueOfHand()) {
-				System.out.println("Dealer wins!");
-				
-			} else {
-				System.out.println("PUSH!!!");
-				
-			}
+	public static void checkForWinner(Player player, Player dealer) {
+		if (player.getPlayerHand().getValueOfHand() > dealer.getPlayerHand().getValueOfHand()) {
+			System.out.println(player.getName() + " " + "Wins!");
+		} else if (player.getPlayerHand().getValueOfHand() < dealer.getPlayerHand().getValueOfHand()) {
+			System.out.println("Dealer Wins!");
+
+		} else {
+			System.out.println("PUSH!!!");
+
 		}
-		public static void showHand(Player p) {
-			for (Card c : p.getPlayerHand().getHand()) {
-				System.out.println(p.getName() + " " + c.getRank() + " " + c.getSuit());
-			}
+	}
+
+	public static void showHand(Player p) {
+		for (Card c : p.getPlayerHand().getHand()) {
+			System.out.println(p.getName() + " " + c.getRank() + " " + c.getSuit());
 		}
+	}
 }
-
-	
-
-
-	
-
